ef6ab24d8e1133aed472b7a8157a83940c9bc570
Krishna Teja <kt@voilacode.com>
2025-09-11 18:51:45 +0530

    changes to code

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/vcwebsite/',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'dist/index.html',
+          dest: '.',
+          rename: '404.html',
+        },
+      ],
+    }),
+  ],
+  base: '/', // Ensure this matches your GitHub repository name or subdirectory
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
@@ -18,6 +30,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
-    minify: false
-  }
-})
+    minify: false,
+  },
+});
