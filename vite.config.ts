421a669dcfc3e13b99ce2aafeb362888d5756f46
Krishna Teja <kt@voilacode.com>
2025-09-21 08:03:14 +0530

    changes to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ const __dirname = path.dirname(__filename)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/', // Ensure this matches your GitHub repository name or subdirectory
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
